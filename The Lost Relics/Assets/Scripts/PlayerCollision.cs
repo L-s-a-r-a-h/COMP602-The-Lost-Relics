49a77d1c190cc716cc4d906a82f993cb6cac13c2
Benjamin501 <dwg3800@autuni.ac.nz>
2022-08-24 20:23:03 +1200

    Coins UI
    Made a UI that displays how many coins have been collected by the player

--- The Lost Relics/Assets/Scripts/PlayerCollision.cs
+++ The Lost Relics/Assets/Scripts/PlayerCollision.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerCollision : MonoBehaviour
 {
+
+    private int coins = 0;
+    public TextMeshProUGUI coinsText;
+
     private void OnTriggerEnter2D(Collider2D collider2D)
     {
         if(collider2D.gameObject.CompareTag("Coin"))
         {
+            coins++;
             Destroy(collider2D.gameObject);
+            coinsText.text = "Coins: " + coins;
+            Debug.Log(coins);
         }
     }
 }
