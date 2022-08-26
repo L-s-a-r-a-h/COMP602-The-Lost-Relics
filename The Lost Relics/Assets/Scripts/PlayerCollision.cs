d2bc82a88221819ef3c46b4fd02236f8c4ec7b1c
Benjamin501 <dwg3800@autuni.ac.nz>
2022-08-26 19:44:31 +1200

    Added collectable coins
    Added a dev scene with coins and a script for player collision to collect them

--- The Lost Relics/Assets/Scripts/PlayerCollision.cs
+++ The Lost Relics/Assets/Scripts/PlayerCollision.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+<<<<<<< Updated upstream
 using TMPro;
+=======
+
+>>>>>>> Stashed changes
 
 public class PlayerCollision : MonoBehaviour
 {
 
+<<<<<<< Updated upstream
     private int coins = 0;
     public TextMeshProUGUI coinsText;
 
@@ -16,6 +21,17 @@ public class PlayerCollision : MonoBehaviour
             coins++;
             Destroy(collider2D.gameObject);
             coinsText.text = "Coins: " + coins;
+=======
+   private int coins = 0;
+   
+
+    private void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if(collider2D.gameObject.CompareTag("coin"))
+        {
+            coins++;
+            Destroy(collider2D.gameObject);
+>>>>>>> Stashed changes
             Debug.Log(coins);
         }
     }
