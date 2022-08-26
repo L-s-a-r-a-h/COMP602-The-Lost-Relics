6ab9c8889c3dba26b4094df3cbeb68c3551ec9ea
Benjamin501 <dwg3800@autuni.ac.nz>
2022-08-26 21:21:06 +1200

    Coin collection update
    Added coin collection with the player model
    
    Tagged Player as player
    
    Created coin object (tagged "Coin")
    
    New PlayerCollision Script -tracks players collision with game objects, assigned it to the player.
    
    Created Currency object to hold all coins

--- The Lost Relics/Assets/Scripts/PlayerCollision.cs
+++ The Lost Relics/Assets/Scripts/PlayerCollision.cs
@@ -1,38 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< Updated upstream
-using TMPro;
-=======
 
->>>>>>> Stashed changes
 
 public class PlayerCollision : MonoBehaviour
 {
 
-<<<<<<< Updated upstream
     private int coins = 0;
-    public TextMeshProUGUI coinsText;
-
-    private void OnTriggerEnter2D(Collider2D collider2D)
-    {
-        if(collider2D.gameObject.CompareTag("Coin"))
-        {
-            coins++;
-            Destroy(collider2D.gameObject);
-            coinsText.text = "Coins: " + coins;
-=======
-   private int coins = 0;
    
 
     private void OnTriggerEnter2D(Collider2D collider2D)
     {
-        if(collider2D.gameObject.CompareTag("coin"))
+        if (collider2D.gameObject.CompareTag("Coin"))
         {
             coins++;
             Destroy(collider2D.gameObject);
->>>>>>> Stashed changes
-            Debug.Log(coins);
+            Debug.Log(" " + coins);
         }
     }
 }
--- The Lost Relics/Assets/Scripts/PlayerMovement.cs
+++ The Lost Relics/Assets/Scripts/PlayerMovement.cs
@@ -1,32 +1,72 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-  
-        PlayerControls controls;
-        float direction = 0;
-
-        public float speed = 400;
-
-        public Rigidbody2D playerRB;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private LayerMask groundLayer;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private BoxCollider2D boxCollider2D;
+    private float horizontalInput;
 
     private void Awake()
-        {
-            controls = new PlayerControls();
-            controls.Enable();
-
-            controls.Land.Move.performed += ctx =>
-            {
-                direction = ctx.ReadValue<float>();
-            };
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            playerRB.velocity = new Vector2(direction * speed * Time.deltaTime, playerRB.velocity.y);
-        }
+    {
+        // Grab References
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+
+        // Flip player depending on direction of movement
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        // Player left/right movement
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+
+
+        // Set Animator Parameters
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", IsGrounded());
+
+    }
+
+    private void Jump()
+    {
+        if (IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            anim.SetTrigger("jump"); // When anims and sprites are chosen.
+        }
+
+    }
+
+    /**
+     * This detects if the player is on the ground. Ground layer must be set.
+     * */
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+
+        return raycastHit.collider != null;
+    }
+
+}
 
