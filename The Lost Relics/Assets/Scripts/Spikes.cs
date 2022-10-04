cf5218538f7d536cf618278039cbf0f47839fd14
Benjamin501 <dwg3800@autuni.ac.nz>
2022-10-04 16:16:05 +1300

    Chest improved
    chest has been improved and some prefabs have been cleaned up

--- The Lost Relics/Assets/Scripts/Spikes.cs
+++ The Lost Relics/Assets/Scripts/Spikes.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class Spikes : MonoBehaviour
 {
+    public float projectileSpeed = 3;
+    private Rigidbody2D rigidody;
     [SerializeField] private float damage = 1;
+
+    void Start()
+    {
+        rigidody = GetComponent<Rigidbody2D>();
+        rigidody.velocity = transform.right * projectileSpeed;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
