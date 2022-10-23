312d511a999dd616d379537637ab1e46bb2180df
Benjamin501 <dwg3800@autuni.ac.nz>
2022-10-23 20:52:39 +1300

    update

--- The Lost Relics/Assets/Scripts/Relics/RelicsCollected.cs
+++ The Lost Relics/Assets/Scripts/Relics/RelicsCollected.cs
@@ -1,23 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class RelicsCollected : MonoBehaviour
 {
     public static int numRelics;
-    public TextMeshProUGUI relicText;
+    [SerializeField] private Image currentRelics;
 
     // start game with 0 coins
     //will update once load function is ready
     private void Awake()
     {
+        currentRelics.fillAmount = numRelics / 10;
         numRelics = 0;
     }
 
     // keeps track of the amount of coins collected and updates the hud.
     void Update()
     {
-        relicText.text = "Relics: " + numRelics;
+        currentRelics.fillAmount = numRelics / 10;
     }
 }
