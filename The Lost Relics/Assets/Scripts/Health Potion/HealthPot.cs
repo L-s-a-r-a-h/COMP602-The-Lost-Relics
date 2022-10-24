80238b162b4f503c80fe4cc3074f05f84dd134d3
Zack <71916918+zakki-miilo@users.noreply.github.com>
2022-10-25 11:12:52 +1300

    More fixes to maxPotion

--- The Lost Relics/Assets/Scripts/Health Potion/HealthPot.cs
+++ The Lost Relics/Assets/Scripts/Health Potion/HealthPot.cs
@@ -26,6 +26,7 @@ public class HealthPot : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.P))
             {
                 healthPotionUse();
+               
             }
         }
     }
