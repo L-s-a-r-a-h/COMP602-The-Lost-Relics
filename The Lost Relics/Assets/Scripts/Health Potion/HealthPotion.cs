d1d88ad8ec8e517dd8fab040d5d8f196ef30a96a
cocoabun <105182251+cocoabun@users.noreply.github.com>
2022-10-24 14:13:13 +1300

    Update save and load for health potions

--- The Lost Relics/Assets/Scripts/Health Potion/HealthPotion.cs
+++ The Lost Relics/Assets/Scripts/Health Potion/HealthPotion.cs
@@ -3,7 +3,15 @@ using UnityEngine;
 public class HealthPotion : MonoBehaviour
 {
     [SerializeField] private float healthValue;
+    private bool collected = false;
+    [SerializeField] private string id;
+    [ContextMenu("Generate guid for id")]
 
+
+    private void GenerateGuid()
+    {
+        id = System.Guid.NewGuid().ToString();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Player")
@@ -12,9 +20,32 @@ public class HealthPotion : MonoBehaviour
 
 
             HealthPot.numberOfPotions++;
-            Destroy(GetComponent<Collider2D>().gameObject);
+            collected = true;
+            this.gameObject.SetActive(false);
+            //  Destroy(GetComponent<Collider2D>().gameObject);
             Debug.Log("HP potion collected");
             
         }
+    }
+    public void LoadData(GameData data)
+    {
+
+        data.coinCollected.TryGetValue(id, out collected);
+
+        if (this.collected)
+        {
+            gameObject.SetActive(false);
+        }
+
+
+    }
+
+    public void SaveData(ref GameData data)
+    {
+        if (data.coinCollected.ContainsKey(id))
+        {
+            data.coinCollected.Remove(id);
+        }
+        data.coinCollected.Add(id, collected);
     }
 }
