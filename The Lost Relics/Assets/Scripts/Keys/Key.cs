c59f650cd088c94bf26f0c85bd51c3ee3b5411d3
cocoabun <105182251+cocoabun@users.noreply.github.com>
2022-10-25 18:49:25 +1300

    Update Key.cs

--- The Lost Relics/Assets/Scripts/Keys/Key.cs
+++ The Lost Relics/Assets/Scripts/Keys/Key.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Key : MonoBehaviour, IDataPercistence
 {
     [SerializeField] private string id;
+    private SpriteRenderer visual;
     private bool collected = false;
     [ContextMenu("Generate guid for id")]
 
@@ -13,7 +14,39 @@ public class Key : MonoBehaviour, IDataPercistence
     {
         id = System.Guid.NewGuid().ToString();
     }
- 
+
+    void Start()
+    {
+        visual = this.GetComponentInChildren<SpriteRenderer>();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //When the player collides with the relic, its added to there invintory
+        //the relic will be destroyed in the scene so it can only be collected once.
+
+        if (collision.transform.tag == "Player")
+        {
+            if (!collected)
+            {
+                CollectKey();
+            }
+
+
+            //  Destroy(GetComponent<Collider2D>().gameObject);
+            //     Debug.Log("Relic Collected");
+
+        }
+    }
+
+    private void CollectKey()
+    {
+        collected = true;
+        visual.gameObject.SetActive(false);
+       Keys.numKeys ++;
+        
+    }
 
     public void LoadData(GameData data)
     {
